63678c1dce6dfea237e74c47e1da5c02b98a97f0
Jack Donohoe <72226393+Jack-Donohoe@users.noreply.github.com>
2022-12-09 11:47:20 +0000

    Caught up to start of week 12

--- ZombieArena/ZombieArena/createBackground.cpp
+++ ZombieArena/ZombieArena/createBackground.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include "zombieArena.h"
 
-int createBackground(VertexArray& rVA, IntRect arena) {
+int createBackground(VertexArray& rVA, IntRect arena)
+{
+
 	// How big is each tile/texture
 	const int TILE_SIZE = 50;
 	const int TILE_TYPES = 3;
@@ -18,26 +20,31 @@ int createBackground(VertexArray& rVA, IntRect arena) {
 
 	// Start at the beginning of the vertex array
 	int currentVertex = 0;
-
 	srand((int)time(0));
+
 
 	for (int w = 0; w < worldWidth; w++) {
 		for (int h = 0; h < worldHeight; h++) {
 			// Position each vertex in the current quad
-			rVA[currentVertex + 0].position = Vector2f(w * TILE_SIZE, h * TILE_SIZE);
-			rVA[currentVertex + 1].position = Vector2f((w * TILE_SIZE) + TILE_SIZE, h * TILE_SIZE);
-			rVA[currentVertex + 2].position = Vector2f((w * TILE_SIZE) + TILE_SIZE, (h * TILE_SIZE) + TILE_SIZE);
-			rVA[currentVertex + 3].position = Vector2f((w * TILE_SIZE), (h * TILE_SIZE) + TILE_SIZE);
+			rVA[currentVertex + 0].position =
+				Vector2f(w * TILE_SIZE, h * TILE_SIZE);
+			rVA[currentVertex + 1].position =
+				Vector2f((w * TILE_SIZE) + TILE_SIZE, h * TILE_SIZE);
+			rVA[currentVertex + 2].position =
+				Vector2f((w * TILE_SIZE) + TILE_SIZE, (h * TILE_SIZE) + TILE_SIZE);
+			rVA[currentVertex + 3].position =
+				Vector2f((w * TILE_SIZE), (h * TILE_SIZE) + TILE_SIZE);
 
 			// Define the position in the Texture to draw for current quad
 			// Either mud, stone, grass or wall
 			if (h == 0 || h == worldHeight - 1 || w == 0 || w == worldWidth - 1) {
 				// Use the wall texture
 				rVA[currentVertex + 0].texCoords = Vector2f(0, 0 + TILE_TYPES * TILE_SIZE);
-				rVA[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, 0 + TILE_SIZE * TILE_TYPES);
+				rVA[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, 0 + TILE_TYPES * TILE_SIZE);
 				rVA[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, TILE_SIZE + TILE_TYPES * TILE_SIZE);
 				rVA[currentVertex + 3].texCoords = Vector2f(0, TILE_SIZE + TILE_TYPES * TILE_SIZE);
 			}
+
 			else {
 				// Use a random floor texture
 				int mOrG = (rand() % TILE_TYPES);
@@ -49,10 +56,13 @@ int createBackground(VertexArray& rVA, IntRect arena) {
 				rVA[currentVertex + 3].texCoords = Vector2f(0, TILE_SIZE + verticalOffset);
 			}
 
-			// Position ready for the next four vertices
+
+
+			// Position ready for the next for vertices
 			currentVertex = currentVertex + VERTS_IN_QUAD;
 		}
 	}
+
 
 	return TILE_SIZE;
 }
--- ZombieArena/ZombieArena/zombieArena.h
+++ ZombieArena/ZombieArena/zombieArena.h
@@ -1,2 +1,8 @@
+##pragma once
+#include "zombie.h"
+
 using namespace sf;
+
 int createBackground(VertexArray& rVA, IntRect arena);
+Zombie* createHorde(int numZombies, IntRect arena);
+
